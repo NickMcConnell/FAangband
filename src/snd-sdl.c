8a72fdaf991f4ba5e8fe0df775db833f2a5252ac
Nick McConnell <nckmccnnll@gmail.com>
2011-04-09 23:17:05 +1000

    Fix SDL sound

--- src/snd-sdl.c
+++ src/snd-sdl.c
@@ -37,7 +37,7 @@ static bool no_cache_audio = FALSE;
 typedef struct
 {
 	int num;                        /* Number of samples for this event */
-	Mix_Chunk *wavs[MAX_SAMPLES];   /* Sample array */
+	Mix_Music *wavs[MAX_SAMPLES];   /* Sample array */
 	char *paths[MAX_SAMPLES]; /* Relative pathnames for samples */
 } sample_list;
 
@@ -64,7 +64,7 @@ static void close_audio(void)
 		/* Nuke all samples */
 		for (j = 0; j < smp->num; j++)
 		{
-			Mix_FreeChunk(smp->wavs[j]);
+			Mix_FreeMusic(smp->wavs[j]);
 			string_free(smp->paths[j]);
 		}
 	}
@@ -218,7 +218,7 @@ static bool sound_sdl_init(bool no_cache)
 			else
 			{
 				/* Load the file now */
-				samples[event].wavs[num] = Mix_LoadWAV(path);
+				samples[event].wavs[num] = Mix_LoadMUS(path);
 				if (!samples[event].wavs[num])
 				{
 					plog_fmt("%s: %s", SDL_GetError(), strerror(errno));
@@ -266,7 +266,7 @@ static bool sound_sdl_init(bool no_cache)
  */
 static void play_sound(int event)
 {
-	Mix_Chunk *wave = NULL;
+	Mix_Music *wave = NULL;
 	int s;
 
 	/* Paranoia */
@@ -287,7 +287,7 @@ static void play_sound(int event)
 		if (!file_exists(filename)) return;
 
 		/* Load */
-		wave = Mix_LoadWAV(filename);
+		wave = Mix_LoadMUS(filename);
 	}
 
 	/* Check to see if we have a wave again */
@@ -298,7 +298,7 @@ static void play_sound(int event)
 	}
 
 	/* Actually play the thing */
-	Mix_PlayChannel(-1, wave, 0);
+	Mix_PlayMusic(wave, 1);
 }
 
 
