92e77bdead246e588c45bc9c1272ac4fe164abae
Nick McConnell <nckmccnnll@gmail.com>
2011-09-08 23:12:36 +1000

    Change to 1.2.4

--- src/defines.h
+++ src/defines.h
@@ -61,10 +61,10 @@
 /**
  * Current version string - according to FAangband reckoning.
  */
-#define VERSION_STRING "1.2.3"
+#define VERSION_STRING "1.2.4"
 /*
 #ifdef BUILD_ID
-#define VERSION_STRING	"1.2.2 (" BUILD_ID ")"
+#define VERSION_STRING	"1.2.4 (" BUILD_ID ")"
 #endif
 */
 
@@ -73,7 +73,7 @@
  */
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	2
-#define VERSION_PATCH	3
+#define VERSION_PATCH	4
 #define VERSION_EXTRA	0
 
 /**
