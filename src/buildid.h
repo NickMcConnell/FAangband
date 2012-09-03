6182afef6832fcaf966f7618a4394ee59eb945c3
Nick McConnell <nckmccnnll@gmail.com>
2012-09-03 22:28:31 +1000

    Move to 1.2.11

--- src/buildid.h
+++ src/buildid.h
@@ -8,7 +8,7 @@
 # define XSTR(x) STR(x)
 # define VERSION_STRING XSTR(BUILD_ID)
 #else
-# define VERSION_STRING "1.2.10"
+# define VERSION_STRING "1.2.11"
 #endif
 
 extern const char *buildid;
--- src/defines.h
+++ src/defines.h
@@ -62,7 +62,7 @@
  */
 /*
 #ifdef BUILD_ID
-#define VERSION_STRING	"1.2.10 (" BUILD_ID ")"
+#define VERSION_STRING	"1.2.11 (" BUILD_ID ")"
 #endif
 */
 
@@ -71,7 +71,7 @@
  */
 #define VERSION_MAJOR	1
 #define VERSION_MINOR	2
-#define VERSION_PATCH	10
+#define VERSION_PATCH	11
 #define VERSION_EXTRA	0
 
 /**
