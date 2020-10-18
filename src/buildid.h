32ef317dd1eda38710c0d43efb808be56bc7e420
Nick McConnell <nckmccnnll@gmail.com>
2020-10-18 14:41:45 +1100

    Update to beta1

--- src/buildid.h
+++ src/buildid.h
@@ -26,7 +26,7 @@
 # define XSTR(x) STR(x)
 # define VERSION_STRING XSTR(BUILD_ID)
 #else
-# define VERSION_STRING "2.0beta"
+# define VERSION_STRING "2.0beta1"
 #endif
 
 extern const char *buildid;
