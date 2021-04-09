7b82a80be63a17c1e7ebf9265552a6713cf72dbd
Nick McConnell <nckmccnnll@gmail.com>
2021-04-10 08:29:42 +1000

    Improvements to effect info

--- src/effects-info.c
+++ src/effects-info.c
@@ -334,7 +334,7 @@ textblock *effect_describe(const struct effect *e, const char *prefix,
 
 		/* Check all the possible types of description format. */
 		switch (base_descs[e->index].efinfo_flag) {
-		case EFINFO_HURT:
+		case EFINFO_DICE:
 			strnfmt(desc, sizeof(desc), edesc, dice_string);
 			break;
 
@@ -438,6 +438,12 @@ textblock *effect_describe(const struct effect *e, const char *prefix,
 			boosted = (dev_skill_boost != 0);
 			break;
 
+		case EFINFO_STAR:
+			strnfmt(desc, sizeof(desc), edesc,
+				e->radius, dice_string);
+			boosted = (dev_skill_boost != 0);
+			break;
+
 		case EFINFO_SPOT:
 			{
 				int i_radius = e->other ? e->other : e->radius;
@@ -485,10 +491,6 @@ textblock *effect_describe(const struct effect *e, const char *prefix,
 		case EFINFO_TOUCH:
 			strnfmt(desc, sizeof(desc), edesc,
 				projections[e->subtype].desc);
-			break;
-
-		case EFINFO_TAP:
-			strnfmt(desc, sizeof(desc), edesc, dice_string);
 			break;
 
 		case EFINFO_NONE:
