92dfc38b38316251f611c647baa1395df42cf133
backwardsEric <55062581+backwardsEric@users.noreply.github.com>
2021-05-15 16:02:09 -0700

    Remove last two uses of atexit() (#4795)
    * Add textui_knowledge_cleanup() to release the resources acquired by textui_knowledge_init(). Call that cleanup function in textui_cleanup(). This removes one of the two remaining uses of atexit() to help resolve https://github.com/angband/angband/issues/2596 .
    
    * Add close_sound() to release any resources allocated by init_sound(). Call that function in main.c and main-win.c. For the SDL sound module, change the close hook so it only shuts down the audio subsystem rather than all of SDL (the previous behavior combined with the use of close_sound() caused crashes when choosing to play a new character rather than exiting from the death screen). Removes the last use of atexit() to resolve https://github.com/angband/angband/issues/2596 .

--- src/ui-knowledge.c
+++ src/ui-knowledge.c
@@ -3229,13 +3229,6 @@ static menu_action knowledge_actions[] =
 
 static struct menu knowledge_menu;
 
-/**
- * Keep macro counts happy.
- */
-static void cleanup_cmds(void) {
-	mem_free(obj_group_order);
-}
-
 void textui_knowledge_init(void)
 {
 	/* Initialize the menus */
@@ -3252,7 +3245,6 @@ void textui_knowledge_init(void)
 		int gid = -1;
 
 		obj_group_order = mem_zalloc((TV_MAX + 1) * sizeof(int));
-		atexit(cleanup_cmds);
 
 		/* Allow for missing values */
 		for (i = 0; i < TV_MAX; i++)
@@ -3264,6 +3256,13 @@ void textui_knowledge_init(void)
 			obj_group_order[object_text_order[i].tval] = gid;
 		}
 	}
+}
+
+
+void textui_knowledge_cleanup(void)
+{
+	mem_free(obj_group_order);
+	obj_group_order = NULL;
 }
 
 
