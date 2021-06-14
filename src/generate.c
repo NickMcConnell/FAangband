2d8a704159a14889624351cd877aaedffd025d86
backwardsEric <55062581+backwardsEric@users.noreply.github.com>
2021-06-13 23:35:21 -0700

    Plug some memory leaks when using persistent levels (#4857)
    * Plug leak of connector linked list during persistent level generation.
    
    * Plug leak of cave name when loading persistent levels.
    
    * Call wipe_mon_list() for chunks in the chunk list so monster group information isn't leaked when persistent levels are used and the player restarts without exiting.

--- src/generate.c
+++ src/generate.c
@@ -1422,6 +1422,38 @@ static void leave_arena(struct chunk *c, struct player *p)
 }
 
 /**
+ * Release the dynamically allocated resources in a dun_data structure.
+ */
+static void cleanup_dun_data(struct dun_data *dd)
+{
+	int i;
+	struct connector *join = dun->join;
+
+	while (join) {
+		struct connector *jtgt = join;
+
+		join = join->next;
+		mem_free(jtgt);
+	}
+	mem_free(dun->cent);
+	mem_free(dun->ent_n);
+	for (i = 0; i < z_info->level_room_max; ++i) {
+		mem_free(dun->ent[i]);
+	}
+	mem_free(dun->ent);
+	if (dun->ent2room) {
+		for (i = 0; dun->ent2room[i]; ++i) {
+			mem_free(dun->ent2room[i]);
+		}
+		mem_free(dun->ent2room);
+	}
+	mem_free(dun->door);
+	mem_free(dun->wall);
+	mem_free(dun->tunn);
+}
+
+
+/**
  * ------------------------------------------------------------------------
  * Main level generation functions
  * ------------------------------------------------------------------------ */
@@ -1491,22 +1523,7 @@ static struct chunk *cave_generate(struct player *p, int height, int width)
 		chunk = dun->profile->builder(p, height, width);
 		if (!chunk) {
 			error = "Failed to find builder";
-			mem_free(dun->join);
-			mem_free(dun->cent);
-			mem_free(dun->ent_n);
-			for (i = 0; i < z_info->level_room_max; ++i) {
-				mem_free(dun->ent[i]);
-			}
-			mem_free(dun->ent);
-			if (dun->ent2room) {
-				for (i = 0; dun->ent2room[i]; ++i) {
-					mem_free(dun->ent2room[i]);
-				}
-				mem_free(dun->ent2room);
-			}
-			mem_free(dun->door);
-			mem_free(dun->wall);
-			mem_free(dun->tunn);
+			cleanup_dun_data(dun);
 			continue;
 		}
 
@@ -1565,22 +1582,7 @@ static struct chunk *cave_generate(struct player *p, int height, int width)
 			cave_free(chunk);
 		}
 
-		mem_free(dun->join);
-		mem_free(dun->cent);
-		mem_free(dun->ent_n);
-		for (i = 0; i < z_info->level_room_max; ++i) {
-			mem_free(dun->ent[i]);
-		}
-		mem_free(dun->ent);
-		if (dun->ent2room) {
-			for (i = 0; dun->ent2room[i]; ++i) {
-				mem_free(dun->ent2room[i]);
-			}
-			mem_free(dun->ent2room);
-		}
-		mem_free(dun->door);
-		mem_free(dun->wall);
-		mem_free(dun->tunn);
+		cleanup_dun_data(dun);
 	}
 
 	if (error) quit_fmt("cave_generate() failed 100 times!");
