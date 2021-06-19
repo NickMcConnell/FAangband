5bf204da5f105a204169ea956ace6c66a3b9c2e9
Nick McConnell <nckmccnnll@gmail.com>
2021-06-19 10:27:15 +1000

    Make it possible to cancel Dimension Door

--- src/effects.c
+++ src/effects.c
@@ -3242,7 +3242,7 @@ static bool effect_handler_TELEPORT_TO(effect_handler_context_t *context)
 
 		/* Player choice */
 		do {
-			get_aim_dir(&dir);
+			if (!get_aim_dir(&dir)) return false;
 		} while (dir == DIR_TARGET && !target_okay());
 
 		if (dir == DIR_TARGET)
