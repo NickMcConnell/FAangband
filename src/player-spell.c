1a78e0eab803380a310caa6dc8af59bfab294da1
Eric Branlund <ebranlund@fastmail.com>
2021-12-30 10:03:54 -0800

    Avoid allocation in spell_cast().

--- src/player-spell.c
+++ src/player-spell.c
@@ -491,8 +491,8 @@ static int beam_chance(void)
 bool spell_cast(int spell_index, int dir, struct command *cmd)
 {
 	int chance;
-	bool *ident = mem_zalloc(sizeof(*ident));
-	int beam = beam_chance();
+	bool ident = false;
+	int beam  = beam_chance();
 	bool failed = false;
 
 	/* Get the spell */
@@ -508,9 +508,8 @@ bool spell_cast(int spell_index, int dir, struct command *cmd)
 		failed = true;
 	} else {
 		/* Cast the spell */
-		if (!effect_do(spell->effect, source_player(), NULL, ident, true, dir,
+		if (!effect_do(spell->effect, source_player(), NULL, &ident, true, dir,
 					   beam, 0, cmd)) {
-			mem_free(ident);
 			return false;
 		}
 
@@ -568,7 +567,6 @@ bool spell_cast(int spell_index, int dir, struct command *cmd)
 	/* Redraw mana */
 	player->upkeep->redraw |= (PR_MANA);
 
-	mem_free(ident);
 	return true;
 }
 
