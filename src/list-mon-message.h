02b7bf2fd88fc7f344bd0debc58c9cf2db11ae98
Eric Branlund <ebranlund@fastmail.com>
2022-01-01 09:30:45 -0800

    Improve "puff of smake" messaging.

--- src/list-mon-message.h
+++ src/list-mon-message.h
@@ -64,6 +64,7 @@ MON_MSG(MAINTAIN_SHAPE,		MSG_GENERIC,	false,	"maintain[s] the same shape.")
 MON_MSG(SAVE_HELD,			MSG_GENERIC,	false,	"fight[s] off your spell.")
 MON_MSG(UNHARMED,			MSG_GENERIC,	false,	"[is|are] unharmed.")
 MON_MSG(APPEAR,			MSG_GENERIC,	false,	"appear[s]!")
+MON_MSG(HIT_AND_RUN,		MSG_GENERIC,	true,	"There is a puff of smoke!")
 /* Dummy messages for monster pain - we use edit file info instead. */
 MON_MSG(95,					MSG_GENERIC,	false,	"")
 MON_MSG(75,					MSG_GENERIC,	false,	"")
