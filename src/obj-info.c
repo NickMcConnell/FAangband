9e529a0a083e1ace59ddad04c950a35dd89ee993
Eric Branlund <ebranlund@fastmail.com>
2022-04-08 10:53:29 -0600

    Fix object information for ranged attacks:  damage is not affected by temporary brands or slays.  Resolves report from Ingwe Ingweron and robinjohnson, http://angband.oook.cz/forum/showpost.php?p=158180&postcount=98 and http://angband.oook.cz/forum/showpost.php?p=158188&postcount=100 .

--- src/obj-info.c
+++ src/obj-info.c
@@ -825,8 +825,11 @@ static bool obj_known_damage(const struct object *obj, int *normal_damage,
 	for (i = 1; i < z_info->brand_max; i++) {
 		int brand_average, add = brands[i].multiplier - 10;
 
-		/* Must have the brand, possibly from a spell */
-		if (player_has_temporary_brand(player, i)) {
+		/*
+		 * Must have the brand, possibly from a spell; temporary brands
+		 * only affect melee attacks.
+		 */
+		if (player_has_temporary_brand(player, i) && !ammo && !throw) {
 			*nonweap_slay = true;
 		} else if (!total_brands[i]) {
 			continue;
@@ -858,8 +861,11 @@ static bool obj_known_damage(const struct object *obj, int *normal_damage,
 	for (i = 1; i < z_info->slay_max; i++) {
 		int slay_average, add = slays[i].multiplier - 10;
 
-		/* Must have the slay, possibly from a spell */
-		if (player_has_temporary_slay(player, i)) {
+		/*
+		 * Must have the slay, possibly from a spell; temporary slays
+		 * only affect melee attacks.
+		 */
+		if (player_has_temporary_slay(player, i) && !ammo && !throw) {
 			*nonweap_slay = true;
 		} else if (!total_slays[i]) {
 			continue;
