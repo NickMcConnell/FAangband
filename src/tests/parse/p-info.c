angband/angband

Extend tests for better coverage of init.c.

src/tests/parse/p-info.c
@@ -1,7 +1,9 @@
 /* parse/p-info */
+/* Exercise parsing used for p_race.txt. */
 
 #include "unit-test.h"
 #include "init.h"
+#include "object.h"
 #include "player.h"
 
 
@@ -16,6 +18,57 @@ int teardown_tests(void *state) {
 	mem_free(pr);
 	parser_destroy(state);
 	return 0;
+}
+
+static int test_missing_record_header0(void *state) {
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+
+	null(pr);
+	r = parser_parse(p, "stats:0:1:-1:1:-1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-disarm-phys:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-disarm-magic:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-device:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-save:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-stealth:1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-search:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-melee:-1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-shoot:5");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-throw:5");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-dig:0");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "hitdie:10");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "exp:120");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "infravision:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "history:4");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "age:24:16");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "height:71:8");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "weight:115:25");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "obj-flags:SUST_DEX");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "player-flags:KNOW_MUSHROOM");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "values:RES_LIGHT[1]");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	ok;
 }
 
 static int test_name0(void *state) {
@@ -224,29 +277,132 @@ static int test_weight0(void *state) {
 }
 
 static int test_obj_flags0(void *state) {
-	enum parser_error r = parser_parse(state, "obj-flags:SUST_DEX");
-	struct player_race *pr;
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	bitflag eflags[OF_SIZE];
 
+	notnull(pr);
+	of_wipe(pr->flags);
+	/* Check that specifying no flags works. */
+	r = parser_parse(p, "obj-flags:");
 	eq(r, PARSE_ERROR_NONE);
-	pr = parser_priv(state);
-	require(pr);
-	require(pr->flags);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(of_is_empty(pr->flags));
+	/* Try one flag. */
+	r = parser_parse(p, "obj-flags:SUST_DEX");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try multiple flags at once. */
+	r = parser_parse(p, "obj-flags:HOLD_LIFE | FREE_ACT");
+	eq(r, PARSE_ERROR_NONE);
+	of_wipe(eflags);
+	of_on(eflags, OF_SUST_DEX);
+	of_on(eflags, OF_HOLD_LIFE);
+	of_on(eflags, OF_FREE_ACT);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(of_is_equal(pr->flags, eflags));
+	ok;
+}
+
+static int test_obj_flags_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized flag. */
+	enum parser_error r = parser_parse(p, "obj-flags:XYZZY");
+
+	eq(r, PARSE_ERROR_INVALID_FLAG);
 	ok;
 }
 
 static int test_play_flags0(void *state) {
-	enum parser_error r = parser_parse(state, "player-flags:KNOW_ZAPPER");
-	struct player_race *pr;
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	bitflag eflags[PF_SIZE];
 
+	notnull(pr);
+	pf_wipe(pr->flags);
+	/* Check that specifying no flags works. */
+	r = parser_parse(p, "player-flags:");
 	eq(r, PARSE_ERROR_NONE);
-	pr = parser_priv(state);
-	require(pr);
-	require(pr->pflags);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_empty(pr->pflags));
+	/* Try one flag. */
+	r  = parser_parse(p, "player-flags:KNOW_ZAPPER");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try setting more than one flag at once. */
+	r = parser_parse(p, "player-flags:SEE_ORE | KNOW_MUSHROOM");
+	eq(r, PARSE_ERROR_NONE);
+	pf_wipe(eflags);
+	pf_on(eflags, PF_KNOW_ZAPPER);
+	pf_on(eflags, PF_SEE_ORE);
+	pf_on(eflags, PF_KNOW_MUSHROOM);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_equal(pr->pflags, eflags));
+	ok;
+}
+
+static int test_play_flags_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized flag. */
+	enum parser_error r = parser_parse(p, "player-flags:XYZZY");
+
+	eq(r, PARSE_ERROR_INVALID_FLAG);
+	ok;
+}
+
+static int test_values0(void *state) {
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	int i;
+
+	notnull(pr);
+	for (i = 0; i < ELEM_MAX; ++i) {
+		pr->el_info[i].res_level = 0;
+	}
+	/* Try setting one value. */
+	r = parser_parse(p, "values:RES_DARK[1]");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try setting multiple values at once. */
+	r = parser_parse(p, "values:RES_FIRE[1] | RES_COLD[-1]");
+	eq(r, PARSE_ERROR_NONE);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	for (i = 0; i < ELEM_MAX; ++i) {
+		if (i == ELEM_DARK || i == ELEM_FIRE) {
+			eq(pr->el_info[i].res_level, 1);
+		} else if (i == ELEM_COLD) {
+			eq(pr->el_info[i].res_level, -1);
+		} else {
+			eq(pr->el_info[i].res_level, 0);
+		}
+	}
+	ok;
+}
+
+static int test_values_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized value. */
+	enum parser_error r = parser_parse(p, "values:XYZZY[2]");
+
+	eq(r, PARSE_ERROR_INVALID_VALUE);
+	/* Try an unrecognized element. */
+	r = parser_parse(p, "values:RES_XYZZY[3]");
+	eq(r, PARSE_ERROR_INVALID_VALUE);
 	ok;
 }
 
 const char *suite_name = "parse/p-info";
+/*
+ * test_missing_header_record0() has to be before test_name0().  All others,
+ * except test_name0(), have to be after test_name0().
+ */
 struct test tests[] = {
+	{ "missing_record_header0", test_missing_record_header0 },
 	{ "name0", test_name0 },
 	{ "stats0", test_stats0 },
 	{ "skill_disarm0", test_skill_disarm0 },
@@ -266,6 +422,10 @@ struct test tests[] = {
 	{ "height0", test_height0 },
 	{ "weight0", test_weight0 },
 	{ "object_flags0", test_obj_flags0 },
+	{ "object_flags_bad0", test_obj_flags_bad0 },
 	{ "player_flags0", test_play_flags0 },
+	{ "player_flags_bad0", test_play_flags_bad0 },
+	{ "values0", test_values0 },
+	{ "values_bad0", test_values_bad0 },
 	{ NULL, NULL }
 };

NickMcConnell/FAangband

src/tests/parse/p-info.c
@@ -1,7 +1,9 @@
 /* parse/p-info */
+/* Exercise parsing used for p_race.txt. */
 
 #include "unit-test.h"
 #include "init.h"
+#include "object.h"
 #include "player.h"
 
 
@@ -16,6 +18,55 @@ int teardown_tests(void *state) {
 	mem_free(pr);
 	parser_destroy(state);
 	return 0;
+}
+
+static int test_missing_record_header0(void *state) {
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+
+	null(pr);
+	r = parser_parse(p, "stats:0:1:-1:1:-1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-disarm-phys:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-disarm-magic:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-device:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-save:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-stealth:1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-search:3");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-melee:-1");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-shoot:5");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-throw:5");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "skill-dig:0");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "hitdie:10");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "infravision:2");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "history:4");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "age:24:16");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "height:71:8");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "weight:115:25");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "obj-flags:SUST_DEX");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "player-flags:KNOW_MUSHROOM");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	r = parser_parse(p, "values:RES_LIGHT[1]");
+	eq(r, PARSE_ERROR_MISSING_RECORD_HEADER);
+	ok;
 }
 
 static int test_name0(void *state) {
@@ -224,29 +275,132 @@ static int test_weight0(void *state) {
 }
 
 static int test_obj_flags0(void *state) {
-	enum parser_error r = parser_parse(state, "obj-flags:SUST_DEX");
-	struct player_race *pr;
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	bitflag eflags[OF_SIZE];
 
+	notnull(pr);
+	of_wipe(pr->flags);
+	/* Check that specifying no flags works. */
+	r = parser_parse(p, "obj-flags:");
 	eq(r, PARSE_ERROR_NONE);
-	pr = parser_priv(state);
-	require(pr);
-	require(pr->flags);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(of_is_empty(pr->flags));
+	/* Try one flag. */
+	r = parser_parse(p, "obj-flags:SUST_DEX");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try multiple flags at once. */
+	r = parser_parse(p, "obj-flags:HOLD_LIFE | FREE_ACT");
+	eq(r, PARSE_ERROR_NONE);
+	of_wipe(eflags);
+	of_on(eflags, OF_SUST_DEX);
+	of_on(eflags, OF_HOLD_LIFE);
+	of_on(eflags, OF_FREE_ACT);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(of_is_equal(pr->flags, eflags));
+	ok;
+}
+
+static int test_obj_flags_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized flag. */
+	enum parser_error r = parser_parse(p, "obj-flags:XYZZY");
+
+	eq(r, PARSE_ERROR_INVALID_FLAG);
 	ok;
 }
 
 static int test_play_flags0(void *state) {
-	enum parser_error r = parser_parse(state, "player-flags:KNOW_MUSHROOM");
-	struct player_race *pr;
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	bitflag eflags[PF_SIZE];
 
+	notnull(pr);
+	pf_wipe(pr->flags);
+	/* Check that specifying no flags works. */
+	r = parser_parse(p, "player-flags:");
 	eq(r, PARSE_ERROR_NONE);
-	pr = parser_priv(state);
-	require(pr);
-	require(pr->pflags);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_empty(pr->pflags));
+	/* Try one flag. */
+	r  = parser_parse(p, "player-flags:BEARSKIN");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try setting more than one flag at once. */
+	r = parser_parse(p, "player-flags:SEE_ORE | KNOW_MUSHROOM");
+	eq(r, PARSE_ERROR_NONE);
+	pf_wipe(eflags);
+	pf_on(eflags, PF_BEARSKIN);
+	pf_on(eflags, PF_SEE_ORE);
+	pf_on(eflags, PF_KNOW_MUSHROOM);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	require(pf_is_equal(pr->pflags, eflags));
+	ok;
+}
+
+static int test_play_flags_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized flag. */
+	enum parser_error r = parser_parse(p, "player-flags:XYZZY");
+
+	eq(r, PARSE_ERROR_INVALID_FLAG);
+	ok;
+}
+
+static int test_values0(void *state) {
+	struct parser *p = (struct parser*) state;
+	struct player_race *pr = (struct player_race*) parser_priv(p);
+	enum parser_error r;
+	int i;
+
+	notnull(pr);
+	for (i = 0; i < ELEM_MAX; ++i) {
+		pr->el_info[i].res_level = 0;
+	}
+	/* Try setting one value. */
+	r = parser_parse(p, "values:RES_DARK[40]");
+	eq(r, PARSE_ERROR_NONE);
+	/* Try setting multiple values at once. */
+	r = parser_parse(p, "values:RES_FIRE[40] | RES_COLD[-30]");
+	eq(r, PARSE_ERROR_NONE);
+	pr = (struct player_race*) parser_priv(p);
+	notnull(pr);
+	for (i = 0; i < ELEM_MAX; ++i) {
+		if (i == ELEM_DARK || i == ELEM_FIRE) {
+			eq(pr->el_info[i].res_level, 60);
+		} else if (i == ELEM_COLD) {
+			eq(pr->el_info[i].res_level, 130);
+		} else {
+			eq(pr->el_info[i].res_level, 0);
+		}
+	}
+	ok;
+}
+
+static int test_values_bad0(void *state) {
+	struct parser *p = (struct parser*) state;
+	/* Try an unrecognized value. */
+	enum parser_error r = parser_parse(p, "values:XYZZY[2]");
+
+	eq(r, PARSE_ERROR_INVALID_VALUE);
+	/* Try an unrecognized element. */
+	r = parser_parse(p, "values:RES_XYZZY[3]");
+	eq(r, PARSE_ERROR_INVALID_VALUE);
 	ok;
 }
 
 const char *suite_name = "parse/p-info";
+/*
+ * test_missing_header_record0() has to be before test_name0().  All others,
+ * except test_name0(), have to be after test_name0().
+ */
 struct test tests[] = {
+	{ "missing_record_header0", test_missing_record_header0 },
 	{ "name0", test_name0 },
 	{ "stats0", test_stats0 },
 	{ "skill_disarm0", test_skill_disarm0 },
@@ -266,6 +420,10 @@ struct test tests[] = {
 	{ "height0", test_height0 },
 	{ "weight0", test_weight0 },
 	{ "object_flags0", test_obj_flags0 },
+	{ "object_flags_bad0", test_obj_flags_bad0 },
 	{ "player_flags0", test_play_flags0 },
+	{ "player_flags_bad0", test_play_flags_bad0 },
+	{ "values0", test_values0 },
+	{ "values_bad0", test_values_bad0 },
 	{ NULL, NULL }
 };