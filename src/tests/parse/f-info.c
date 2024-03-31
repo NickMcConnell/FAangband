0bc20d527b8b5022e8cf84f12c2efd62d768199b
Eric Branlund <ebranlund@fastmail.com>
2024-03-31 07:12:30 -0600

    Reduce errors and warnings with Mingw cross-compile of test cases on Linux

--- src/tests/parse/f-info.c
+++ src/tests/parse/f-info.c
@@ -6,8 +6,12 @@
 #include "cave.h"
 #include "init.h"
 #include "monster.h"
+#include "player.h"
+#include "z-form.h"
+#ifndef WINDOWS
 #include <locale.h>
 #include <langinfo.h>
+#endif
 
 
 int setup_tests(void **state) {
@@ -122,6 +126,7 @@ static int test_graphics0(void *state) {
 	notnull(f);
 	eq(f->d_char, L'#');
 	eq(f->d_attr, COLOUR_L_PURPLE);
+#ifndef WINDOWS
 	if (setlocale(LC_CTYPE, "") && streq(nl_langinfo(CODESET), "UTF-8")) {
 		/*
 		 * Check for glyph that is outside of the ASCII range.  Use
@@ -139,6 +144,7 @@ static int test_graphics0(void *state) {
 		eq(f->d_char, wcs[0]);
 		eq(f->d_attr, COLOUR_RED);
 	}
+#endif
 	ok;
 }
 
