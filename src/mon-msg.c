NickMcConnell/FAangband

commit febb4aed296bcef0bc374d604715ad2c19b8892f

Improve the show_damage option
For player damage to monsters, provide damage notifications for projections and effects that do not use projections.
For damage to a player, provide damage notifications for all but the poison, cut, and starvation damage applied in process_world().  The damage notifications now include the effects of player invulnerability, player damage reduction, and changes to damage in monsters' blow handlers.
Resolves https://github.com/angband/angband/issues/5862 and https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=780770 .
Side effect of these changes are:
1) Messages for monsters affected by an earthquake now use add_monster_message() and that implies some changes to the text of the messages, ordering of the messages, and handling of duplicate message.
2) To have the damage notifications for damage to the player include the effects of damage reduction and invulnerability, move the handling of those effects from take_hit() to a new function, player_apply_damage_reduction(), also declared in player-util.h.  Callers of take_hit() should process the damage passed to it with player_apply_damage_reduction() in order to match the old behavior of take_hit().
3) To have the damage notification from a monster blow include the changes to damage in the blow handler and the player's damage reduction, move the messaging about the blow from make_attack_normal() or monster_attack_monster() into the blow handler.  The private functions, display_blow_message_vs_player() and display_blow_message_vs_monster(), in mon-blows.c can handle the messaging for a blow handler.  A blow handler that calls monster_damage_target(), melee_effect_elemental(), or melee_effect_experience() will have the messaging done by those functions.  Blow handlers that would need changes are those that do not call those functions or those that call more than one of them (they would get multiple messages for the same blow).

src/mon-msg.c
@@ -31,10 +31,13 @@
 #define MAX_STORED_MON_CODES	400
 
 /**
- * Flags for whether monsters are offscreen or invisible
+ * Flags to track whether affected monsters are offscreen, affected monsters
+ * are invisible, or the message should include a numerical indication of the
+ * damage
  */
 #define MON_MSG_FLAG_OFFSCREEN	0x01
 #define MON_MSG_FLAG_INVISIBLE	0x02
+#define MON_MSG_FLAG_DAMAGE	0x04
 
 /**
  * A stacked monster message entry
@@ -42,10 +45,12 @@
 struct monster_race_message {
 	struct monster_race *race;	/* The race of the monster */
 	struct player_race *p_race;	/* The player race of the monster */
-	int flags;					/* Flags */
-	int msg_code;				/* The coded message */
-	int count;					/* How many monsters triggered this message */
-	int delay;					/* messages will be processed in this order: delay = 0, 1, 2 */
+	int flags;			/* Flags */
+	int msg_code;			/* The coded message */
+	int count;			/* How many monsters triggered this message */
+	int delay;			/* messages will be processed in this order: delay = 0, 1, 2 */
+	int damage;			/* total damage; only relevant if flags
+						includes MON_MSG_FLAG_DAMAGE */
 };
 
 /**
@@ -86,10 +91,10 @@ static const struct {
 };
 
 /**
- * Adds to the message queue a message describing a monster's reaction
- * to damage.
+ * Help message_pain() and message_pain_show_damage():  get the appropiate
+ * message code for the amount of damage.
  */
-void message_pain(struct monster *mon, int dam)
+static int get_pain_msg_code(struct monster *mon, int dam)
 {
 	int msg_code = MON_MSG_UNHARMED;
 
@@ -107,12 +112,34 @@ void message_pain(struct monster *mon, int dam)
 		else if (percentage > 35)	msg_code = MON_MSG_35;
 		else if (percentage > 20)	msg_code = MON_MSG_20;
 		else if (percentage > 10)	msg_code = MON_MSG_10;
-		else						msg_code = MON_MSG_0;
+		else				msg_code = MON_MSG_0;
 	}
-
-	add_monster_message(mon, msg_code, false);
+	return msg_code;
 }
 
+/**
+ * Adds to the message queue a message describing a monster's reaction
+ * to damage.
+ */
+void message_pain(struct monster *mon, int dam)
+{
+	add_monster_message(mon, get_pain_msg_code(mon, dam), false);
+}
+
+/**
+ * Adds to the message queue a message describing a monster's reaction
+ * to damage which also includes a numerical indication of the damage amount.
+ */
+void message_pain_show_damage(struct monster *mon, int dam)
+{
+	int msg_code = get_pain_msg_code(mon, dam);
+
+	if (dam > 0) {
+		add_monster_message_show_damage(mon, msg_code, false, dam);
+	} else {
+		add_monster_message(mon, msg_code, false);
+	}
+}
 /**
  * Tracks which monster has had which pain message stored, so redundant
  * messages don't happen due to monster attacks hitting other monsters.
@@ -171,7 +198,8 @@ static void store_monster(struct monster *mon, int msg_code)
  *
  * \returns true if successful, false if failed
  */
-static bool stack_message(struct monster *mon, int msg_code, int flags)
+static bool stack_message(struct monster *mon, int msg_code, int flags,
+		int damage)
 {
 	int i;
 
@@ -182,6 +210,25 @@ static bool stack_message(struct monster *mon, int msg_code, int flags)
 			mon_msg[i].flags == flags &&
 			mon_msg[i].msg_code == msg_code) {
 			mon_msg[i].count++;
+			if (flags & MON_MSG_FLAG_DAMAGE) {
+				if (damage >= 0) {
+					if (mon_msg[i].damage <= 0
+							|| mon_msg[i].damage <
+							INT_MAX - damage) {
+						mon_msg[i].damage += damage;
+					} else {
+						mon_msg[i].damage = INT_MAX;
+					}
+				} else {
+					if (mon_msg[i].damage >= 0
+							|| mon_msg[i].damage >
+							INT_MIN - damage) {
+						mon_msg[i].damage += damage;
+					} else {
+						mon_msg[i].damage = INT_MIN;
+					}
+				}
+			}
 			store_monster(mon, msg_code);
 			return true;
 		}
@@ -206,15 +253,15 @@ static int what_delay(int msg_code, int delay)
  */
 bool add_monster_message(struct monster *mon, int msg_code, bool delay)
 {
+	int flags = message_flags(mon);
+
 	assert(msg_code >= 0);
 	assert(msg_code < MON_MSG_MAX);
-
-	int flags = message_flags(mon);
 
 	/* Try to stack the message on top of older messages if it isn't redunant */
 	/* If not possible, check we have storage space for more messages and add */
 	if (!redundant_monster_message(mon, msg_code) &&
-			!stack_message(mon, msg_code, flags) &&
+			!stack_message(mon, msg_code, flags, 0) &&
 			size_mon_msg < MAX_STORED_MON_MSG) {
 		mon_msg[size_mon_msg].race = mon->race;
 		mon_msg[size_mon_msg].p_race = mon->player_race;
@@ -222,6 +269,42 @@ bool add_monster_message(struct monster *mon, int msg_code, bool delay)
 		mon_msg[size_mon_msg].msg_code = msg_code;
 		mon_msg[size_mon_msg].count = 1;
 		mon_msg[size_mon_msg].delay = what_delay(msg_code, delay);
+		mon_msg[size_mon_msg].damage = 0;
+		size_mon_msg++;
+
+		store_monster(mon, msg_code);
+
+		player->upkeep->notice |= PN_MON_MESSAGE;
+
+		return true;
+	} else {
+		return false;
+	}
+}
+
+/**
+ * Stack a codified message with a numerical damage notification for the
+ * given monste race.
+ *
+ * Return true on success.
+ */
+bool add_monster_message_show_damage(struct monster *mon, int msg_code,
+		bool delay, int damage)
+{
+	int flags = message_flags(mon) | MON_MSG_FLAG_DAMAGE;
+
+	assert(msg_code >= 0);
+	assert(msg_code < MON_MSG_MAX);
+
+	if (!redundant_monster_message(mon, msg_code) &&
+			!stack_message(mon, msg_code, flags, damage) &&
+			size_mon_msg < MAX_STORED_MON_MSG) {
+		mon_msg[size_mon_msg].race = mon->race;
+		mon_msg[size_mon_msg].flags = flags;
+		mon_msg[size_mon_msg].msg_code = msg_code;
+		mon_msg[size_mon_msg].count = 1;
+		mon_msg[size_mon_msg].delay = what_delay(msg_code, delay);
+		mon_msg[size_mon_msg].damage = damage;
 		size_mon_msg++;
 
 		store_monster(mon, msg_code);
@@ -392,6 +475,7 @@ static void show_message(struct monster_race_message *msg)
 {
 	char subject[60] = "";
 	char body[60];
+	int msg_type = get_message_type(msg->msg_code, msg->race);
 
 	/* Some messages don't require a monster name */
 	if (!skip_subject(msg->msg_code)) {
@@ -411,10 +495,18 @@ static void show_message(struct monster_race_message *msg)
 			msg->count > 1);
 
 	/* Show the message */
-	msgt(get_message_type(msg->msg_code, msg->race),
-			"%s%s",
-			subject,
-			body);
+	if (msg->flags & MON_MSG_FLAG_DAMAGE) {
+		if (msg->count <= 1) {
+			msgt(msg_type, "%s%s (%d)", subject, body, msg->damage);
+		} else {
+			msgt(msg_type, "%s%s (average %d)", subject, body,
+				msg->damage / msg->count
+				+ (msg->damage % msg->count
+				>= (msg->count + 1) / 2 ? 1 : 0));
+		}
+	} else {
+		msgt(msg_type, "%s%s", subject, body);
+	}
 }
 
 /**

src/effect-handler-attack.c
@@ -542,6 +542,10 @@ bool effect_handler_DAMAGE(effect_handler_context_t *context)
 	}
 
 	/* Hit the player */
+	dam = player_apply_damage_reduction(player, dam);
+	if (dam && OPT(player, show_damage)) {
+		msg("You take %d damage.", dam);
+	}
 	take_hit(player, dam, killer);
 
 	return true;
@@ -1645,6 +1649,8 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 	int safe_grids = 0;
 	int damage = 0;
 	bool hurt = false;
+	bool display_dam = context->origin.what == SRC_PLAYER
+		&& OPT(player, show_damage);
 	bool map[32][32];
 
 	struct loc centre = origin_get_loc(context->origin);
@@ -1719,6 +1725,8 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 
 	/* First, determine the effects on the player (if necessary) */
 	if (hurt) {
+		char dam_text[32] = "";
+
 		/* Check around the player */
 		for (i = 0; i < 8; i++) {
 			/* Get the location */
@@ -1761,31 +1769,44 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 		/* Hurt the player a lot */
 		if (!safe_grids) {
 			/* Message and damage */
-			msg("You are severely crushed!");
-			damage = 300;
+			damage = player_apply_damage_reduction(player, 300);
+			if (damage > 0 && OPT(player, show_damage)) {
+				strnfmt(dam_text, sizeof(dam_text), " (%d)",
+					damage);
+			}
+			msg("You are severely crushed!%s", dam_text);
 		} else {
 			/* Destroy the grid, and push the player to (relative) safety */
+			const char *hurt_msg = "";
+
 			switch (randint1(3)) {
 				case 1: {
-					msg("You nimbly dodge the blast!");
+					hurt_msg = "You nimbly dodge the blast!";
 					damage = 0;
 					break;
 				}
 				case 2: {
-					msg("You are bashed by rubble!");
+					hurt_msg = "You are bashed by rubble!";
 					damage = damroll(10, 4);
 					(void)player_inc_timed(player, TMD_STUN,
 						randint1(50), true, true, true);
 					break;
 				}
 				case 3: {
-					msg("You are crushed between the floor and ceiling!");
+					hurt_msg = "You are crushed between the floor and ceiling!";
 					damage = damroll(10, 4);
 					(void)player_inc_timed(player, TMD_STUN,
 						randint1(50), true, true, true);
 					break;
 				}
 			}
+
+			damage = player_apply_damage_reduction(player, damage);
+			if (damage > 0 && OPT(player, show_damage)) {
+				strnfmt(dam_text, sizeof(dam_text), " (%d)",
+					damage);
+			}
+			msg("%s%s", hurt_msg, dam_text);
 
 			/* Move player */
 			monster_swap(pgrid, safe_grid);
@@ -1810,7 +1831,6 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 				/* Most monsters cannot co-exist with rock */
 				if (!flags_test(mon->race->flags, RF_SIZE, RF_KILL_WALL,
 								RF_PASS_WALL, FLAG_END)) {
-					char m_name[80];
 					int m_dam;
 
 					/* Assume not safe */
@@ -1843,38 +1863,53 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 						}
 					}
 
-					/* Describe the monster */
-					monster_desc(m_name, sizeof(m_name), mon, MDESC_STANDARD);
-
-					/* Scream in pain */
-					msg("%s wails out in pain!", m_name);
-
 					/* Take damage from the quake */
 					m_dam = (safe_grids ? damroll(4, 8) : (mon->hp + 1));
 
 					/* Monster is certainly awake, not thinking about player */
 					monster_wake(mon, false, 0);
 
-					/* If the quake finished the monster off, show message */
-					if (mon->hp < m_dam && mon->hp >= 0)
-						msg("%s is embedded in the rock!", m_name);
-
 					/* Apply damage directly */
 					mon->hp -= m_dam;
 
-					/* Delete (not kill) "dead" monsters */
 					if (mon->hp < 0) {
-						/* Delete the monster */
+						if (display_dam) {
+							add_monster_message_show_damage(
+								mon,
+								MON_MSG_QUAKE_DEATH,
+								false,
+								m_dam);
+						} else {
+							add_monster_message(mon,
+								MON_MSG_QUAKE_DEATH,
+								false);
+						}
+
+						/*
+						 * Delete (not kill) "dead"
+						 * monsters.
+						 */
 						delete_monster(grid);
+					} else {
+						if (display_dam) {
+							add_monster_message_show_damage(
+								mon,
+								MON_MSG_QUAKE_HURT,
+								false,
+								m_dam);
+						} else {
+							add_monster_message(mon,
+								MON_MSG_QUAKE_HURT,
+								false);
+						}
 
-						/* No longer safe */
-						safe_grids = 0;
+						/* Escape from the rock */
+						if (safe_grids) {
+							/* Move the monster */
+							monster_swap(grid,
+								safe_grid);
+						}
 					}
-
-					/* Escape from the rock */
-					if (safe_grids)
-						/* Move the monster */
-						monster_swap(grid, safe_grid);
 				}
 			}
 		}
@@ -1912,7 +1947,7 @@ bool effect_handler_EARTHQUAKE(effect_handler_context_t *context)
 	 * Apply damage to player; done here so messages are ordered properly
 	 * if the player dies.
 	 */
-	if (damage) take_hit(player, damage, "an earthquake");
+	take_hit(player, damage, "an earthquake");
 
 	/* Fully update the visuals */
 	player->upkeep->update |= (PU_UPDATE_VIEW | PU_MONSTERS);
@@ -2060,9 +2095,13 @@ bool effect_handler_TAP_UNLIFE(effect_handler_context_t *context)
 	mon = target_get_monster();
 
 	/* Hurt the monster */
-	monster_desc(m_name, sizeof(m_name), mon, MDESC_TARG);
-	msg("You draw power from the %s.", m_name);
 	drain = MIN(mon->hp, amount) / 4;
+	monster_desc(m_name, sizeof(m_name), mon, MDESC_TARG);
+	if (OPT(player, show_damage)) {
+		msg("You draw power from the %s. (%d)", m_name, drain);
+	} else {
+		msg("You draw power from the %s.", m_name);
+	}
 	dead = mon_take_hit(mon, player, amount, &fear, " is destroyed!");
 
 	/* Gain mana */
@@ -2092,6 +2131,9 @@ bool effect_handler_CURSE(effect_handler_context_t *context)
 	struct monster *mon = target_get_monster();
 	bool fear = false;
 	bool dead = false;
+	bool display_dam = OPT(player, show_damage);
+	char note[32];
+	const char *passed_note;
 
 	context->ident = true;
 
@@ -2102,11 +2144,21 @@ bool effect_handler_CURSE(effect_handler_context_t *context)
 	}
 
 	/* Hit it */
-	dead = mon_take_hit(mon, player, dam, &fear, " dies!");
+	if (display_dam) {
+		strnfmt(note, sizeof(note), " dies! (%d)", dam);
+		passed_note = note;
+	} else {
+		passed_note = " dies!";
+	}
+	dead = mon_take_hit(mon, player, dam, &fear, passed_note);
 
 	/* Handle fear for surviving monsters */
 	if (!dead && monster_is_visible(mon)) {
-		message_pain(mon, dam);
+		if (display_dam) {
+			message_pain_show_damage(mon, dam);
+		} else {
+			message_pain(mon, dam);
+		}
 		if (fear) {
 			add_monster_message(mon, MON_MSG_FLEE_IN_TERROR, true);
 		}

src/effect-handler-general.c
@@ -207,7 +207,15 @@ static bool uncurse_object(struct object *obj, int strength, char *dice_string)
 			/* Failure - unlucky fragile object is destroyed */
 			struct object *destroyed;
 			bool none_left = false;
-			msg("There is a bang and a flash!");
+			int dam = damroll(5, 5);
+			char dam_text[16] = "";
+
+			dam = player_apply_damage_reduction(player, dam);
+			if (dam > 0 && OPT(player, show_damage)) {
+				strnfmt(dam_text, sizeof(dam_text), " (%d)",
+					dam);
+			}
+			msg("%s%s", "There is a bang and a flash!", dam_text);
 			if (object_is_carried(player, obj)) {
 				destroyed = gear_object_for_use(player, obj,
 					1, false, &none_left);
@@ -220,7 +228,7 @@ static bool uncurse_object(struct object *obj, int strength, char *dice_string)
 			} else {
 				square_delete_object(cave, obj->grid, obj, true, true);
 			}
-			take_hit(player, damroll(5, 5), "Failed uncursing");
+			take_hit(player, dam, "Failed uncursing");
 		} else {
 			/* Non-destructive failure */
 			msg("The removal fails.");
@@ -843,14 +851,20 @@ bool effect_handler_DRAIN_STAT(effect_handler_context_t *context)
 	/* Attempt to reduce the stat */
 	if (player_stat_dec(player, stat, false)) {
 		int dam = effect_calculate_value(context, false);
+		char dam_text[32] = "";
+
+		dam = player_apply_damage_reduction(player, dam);
 
 		/* Notice effect */
 		equip_learn_flag(player, flag);
 
 		/* Message */
-		msgt(MSG_DRAIN_STAT, "You feel very %s.", desc_stat(stat, false));
-		if (dam)
-			take_hit(player, dam, "stat drain");
+		if (dam > 0 && OPT(player, show_damage)) {
+			strnfmt(dam_text, sizeof(dam_text), " (%d)", dam);
+		}
+		msgt(MSG_DRAIN_STAT, "You feel very %s.%s",
+			desc_stat(stat, false), dam_text);
+		take_hit(player, dam, "stat drain");
 	}
 
 	return (true);
@@ -2478,6 +2492,10 @@ bool effect_handler_BANISH(effect_handler_context_t *context)
 	}
 
 	/* Hurt the player */
+	dam = player_apply_damage_reduction(player, dam);
+	if (dam > 0 && OPT(player, show_damage)) {
+		msg("You take %d damage.\n", dam);
+	}
 	take_hit(player, dam, "the strain of casting Banishment");
 
 	/* Update monster list window */
@@ -2526,6 +2544,10 @@ bool effect_handler_MASS_BANISH(effect_handler_context_t *context)
 	}
 
 	/* Hurt the player */
+	dam = player_apply_damage_reduction(player, dam);
+	if (dam > 0 && OPT(player, show_damage)) {
+		msg("You take %d damage.\n", dam);
+	}
 	take_hit(player, dam, "the strain of casting Mass Banishment");
 
 	/* Update monster list window */

src/game-world.c
@@ -657,7 +657,8 @@ void process_world(struct chunk *c)
 
 	/* Take damage from poison */
 	if (player->timed[TMD_POISONED]) {
-		take_hit(player, 1, "poison");
+		take_hit(player, player_apply_damage_reduction(player, 1),
+			"poison");
 		if (player->is_dead) {
 			return;
 		}
@@ -678,7 +679,8 @@ void process_world(struct chunk *c)
 		}
 
 		/* Take damage */
-		take_hit(player, i, "a fatal wound");
+		take_hit(player, player_apply_damage_reduction(player, i),
+			"a fatal wound");
 		if (player->is_dead) {
 			return;
 		}
@@ -778,7 +780,8 @@ void process_world(struct chunk *c)
 		i = (PY_FOOD_STARVE - player->timed[TMD_FOOD]) / 10;
 
 		/* Take damage */
-		take_hit(player, i, "starvation");
+		take_hit(player, player_apply_damage_reduction(player, i),
+			"starvation");
 		if (player->is_dead) {
 			return;
 		}

src/list-mon-message.h
@@ -65,6 +65,8 @@ MON_MSG(SAVE_HELD,			MSG_GENERIC,	false,	"fight[s] off your spell.")
 MON_MSG(UNHARMED,			MSG_GENERIC,	false,	"[is|are] unharmed.")
 MON_MSG(APPEAR,			MSG_GENERIC,	false,	"appear[s]!")
 MON_MSG(HIT_AND_RUN,		MSG_GENERIC,	true,	"There is a puff of smoke!")
+MON_MSG(QUAKE_DEATH,		MSG_KILL,	false,	"[is|are] embedded in rock!")
+MON_MSG(QUAKE_HURT,		MSG_GENERIC,	false,	"wail[s] out in pain!")
 /* Dummy messages for monster pain - we use edit file info instead. */
 MON_MSG(95,					MSG_GENERIC,	false,	"")
 MON_MSG(75,					MSG_GENERIC,	false,	"")

src/mon-attack.c
@@ -654,9 +654,6 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 		int damage = 0;
 		bool do_cut = false;
 		bool do_stun = false;
-		int sound_msg = MSG_GENERIC;
-
-		char *act = NULL;
 
 		/* Extract the attack infomation */
 		struct blow_effect *effect = mon->race->blow[ap_cnt].effect;
@@ -682,11 +679,8 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 			/* Sometimes we can avoid the blow */
 			if (player_avoid_blow(mon, m_name, p)) continue;
 
-			/* Describe the attack method */
-			act = monster_blow_method_action(method, -1);
 			do_cut = method->cut;
 			do_stun = method->stun;
-			sound_msg = method->msgt;
 
 			/* Roll dice */
 			damage = randcalc(dice, rlev, RANDOMISE);
@@ -694,21 +688,6 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 			/* Reduce damage when stunned */
 			if (mon->m_timed[MON_TMD_STUN]) {
 				damage = (damage * (100 - STUN_DAM_REDUCTION)) / 100;
-			}
-
-			/* Message */
-			if (act) {
-				const char *fullstop = ".";
-				if (suffix(act, "'") || suffix(act, "!")) {
-					fullstop = "";
-				}
-
-				if (OPT(p, show_damage)) {
-					msgt(sound_msg, "%s %s (%d)%s", m_name, act, damage,
-						 fullstop);
-				} else {
-					msgt(sound_msg, "%s %s%s", m_name, act, fullstop);
-				}
 			}
 
 			/* Perform the actual effect. */
@@ -725,6 +704,7 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 					obvious,
 					blinked,
 					damage,
+					m_name
 				};
 
 				effect_handler(&context);
@@ -801,8 +781,6 @@ bool make_attack_normal(struct monster *mon, struct player *p)
 						true, true, true);
 				}
 			}
-
-			string_free(act);
 		} else {
 			/* Visible monster missed player, so notify if appropriate. */
 			if (monster_is_visible(mon) &&	method->miss) {
@@ -875,9 +853,6 @@ bool monster_attack_monster(struct monster *mon, struct monster *t_mon)
 
 		int damage = 0;
 		bool do_stun = false;
-		int sound_msg = MSG_GENERIC;
-
-		char *act = NULL;
 
 		/* Extract the attack infomation */
 		struct blow_effect *effect = mon->race->blow[ap_cnt].effect;
@@ -893,10 +868,7 @@ bool monster_attack_monster(struct monster *mon, struct monster *t_mon)
 			test_hit(chance_of_monster_hit(mon, effect), t_mon->race->ac)) {
 			melee_effect_handler_f effect_handler;
 
-			/* Describe the attack method */
-			act = monster_blow_method_action(method, t_mon->midx);
 			do_stun = method->stun;
-			sound_msg = method->msgt;
 
 			/* Hack -- assume all attacks are obvious */
 			obvious = true;
@@ -907,16 +879,6 @@ bool monster_attack_monster(struct monster *mon, struct monster *t_mon)
 			/* Reduce damage when stunned */
 			if (mon->m_timed[MON_TMD_STUN]) {
 				damage = (damage * (100 - STUN_DAM_REDUCTION)) / 100;
-			}
-
-			/* Message */
-			if (act) {
-				const char *fullstop = ".";
-				if (suffix(act, "'") || suffix(act, "!")) {
-					fullstop = "";
-				}
-
-				msgt(sound_msg, "%s %s%s", m_name, act, fullstop);
 			}
 
 			/* Perform the actual effect. */
@@ -933,6 +895,7 @@ bool monster_attack_monster(struct monster *mon, struct monster *t_mon)
 					obvious,
 					blinked,
 					damage,
+					m_name
 				};
 
 				effect_handler(&context);
@@ -966,8 +929,6 @@ bool monster_attack_monster(struct monster *mon, struct monster *t_mon)
 				if (amt)
 					(void)mon_inc_timed(t_mon, MON_TMD_STUN, amt, 0);
 			}
-
-			string_free(act);
 		} else {
 			/* Visible monster missed monster, so notify if appropriate. */
 			if (monster_is_visible(mon) && method->miss) {

src/mon-blows.c
@@ -71,7 +71,7 @@ static blow_tag_t blow_tag_lookup(const char *tag)
  * We fill in the monster name and/or pronoun where necessary in
  * the message to replace instances of {name} or {pronoun}.
  */
-char *monster_blow_method_action(struct blow_method *method, int midx)
+char *monster_blow_method_action(const struct blow_method *method, int midx)
 {
 	const char punct[] = ".!?;:,'";
 	char buf[1024] = "\0";
@@ -83,7 +83,7 @@ char *monster_blow_method_action(struct blow_method *method, int midx)
 	struct monster *t_mon = NULL;
 
 	int choice = randint0(method->num_messages);
-	struct blow_message *msg = method->messages;
+	const struct blow_message *msg = method->messages;
 
 	/* Get the target monster, if any */
 	if (midx > 0) {
@@ -181,6 +181,61 @@ int blow_index(const char *name)
 			return i;
 	}
 	return 0;
+}
+
+/**
+ * Display the message for a blow against a player.
+ *
+ * \param method is the structure describing the type of blow.
+ * \param m_name is the formatted name for the attacking monster.
+ * \param p is the player being attacked.
+ * \param damage is the amount of damage from the blow.
+ */
+static void display_blow_message_vs_player(const struct blow_method *method,
+		const char *m_name, struct player *p, int damage)
+{
+	char *act = monster_blow_method_action(method, -1);
+
+	if (act) {
+		const char *fullstop = ".";
+
+		if (suffix(act, "'") || suffix(act, "!")) {
+			fullstop = "";
+		}
+		if (damage > 0 && OPT(p, show_damage)) {
+			msgt(method->msgt, "%s %s%s (%d)", m_name, act,
+				fullstop, damage);
+		} else {
+			msgt(method->msgt, "%s %s%s", m_name, act, fullstop);
+		}
+		string_free(act);
+	} else if (damage > 0 && OPT(p, show_damage)) {
+		msgt(method->msgt, "You take %d damage.", damage);
+	}
+}
+
+/**
+ * Display the message for a blow against another monster.
+ *
+ * \param method is the structure describing the type of blow.
+ * \param m_name is the formatted name for the attacking monster.
+ * \param t_idx is the index for the targeted monster (i.e. if mon is the
+ * structure representing the target, it is mon->midx).
+ */
+static void display_blow_message_vs_monster(const struct blow_method *method,
+		const char *m_name, int t_idx)
+{
+	char *act = monster_blow_method_action(method, t_idx);
+
+	if (act) {
+		const char *fullstop = ".";
+
+		if (suffix(act, "'") || suffix(act, "!")) {
+			fullstop = "";
+		}
+		msgt(method->msgt, "%s %s%s", m_name, act, fullstop);
+		string_free(act);
+	}
 }
 
 /**
@@ -323,12 +378,24 @@ static bool monster_damage_target(melee_effect_handler_context_t *context,
 {
 	/* Take damage */
 	if (context->p) {
-		take_hit(context->p, context->damage, context->ddesc);
+		/*
+		 * Player damage reduction does not affect the damage used for
+		 * side effect calculations so leave context->damage as is.
+		 */
+		int reduced = player_apply_damage_reduction(context->p,
+			context->damage);
+
+		display_blow_message_vs_player(context->method, context->m_name,
+			context->p, reduced);
+		take_hit(context->p, reduced, context->ddesc);
 		if (context->p->is_dead) return true;
 	} else {
-		bool dead = false;
+		bool dead;
+
+		display_blow_message_vs_monster(context->method,
+			context->m_name, context->t_mon->midx);
 		dead = mon_take_nonplayer_hit(context->damage, context->t_mon,
-									  MON_MSG_NONE,	MON_MSG_DIE);
+			MON_MSG_NONE, MON_MSG_DIE);
 		return (dead || no_further_monster_effect);
 	}
 	return false;
@@ -394,10 +461,22 @@ static void melee_effect_elemental(melee_effect_handler_context_t *context,
 		inven_damage(context->p, type, MIN(elemental_dam * 5, 300));
 	if (context->damage > 0) {
 		if (context->p) {
-			take_hit(context->p, context->damage, context->ddesc);
+			/*
+			 * Player damage reduction does not affect the damage]				 * used for side effect calculations so leave
+			 * context->damage as is.
+			 */
+			int reduced = player_apply_damage_reduction(context->p,
+				context->damage);
+
+			display_blow_message_vs_player(context->method,
+				context->m_name, context->p, reduced);
+			take_hit(context->p, reduced, context->ddesc);
 		} else {
-			(void) mon_take_nonplayer_hit(context->damage, context->t_mon,
-										  hurt_msg, die_msg);
+			assert(context->t_mon);
+			display_blow_message_vs_monster(context->method,
+				context->m_name, context->t_mon->midx);
+			(void) mon_take_nonplayer_hit(context->damage,
+				context->t_mon, hurt_msg, die_msg);
 		}
 	}
 
@@ -512,11 +591,23 @@ static void melee_effect_experience(melee_effect_handler_context_t *context,
 {
 	/* Take damage */
 	if (context->p) {
-		take_hit(context->p, context->damage, context->ddesc);
+		/*
+		 * Player damage reduction does not affect the damage used for
+		 * side effect calculations so leave context->damage as is.
+		 */
+		int reduced = player_apply_damage_reduction(context->p,
+			context->damage);
+
+		display_blow_message_vs_player(context->method,
+			context->m_name, context->p, reduced);
+		take_hit(context->p, reduced, context->ddesc);
 		context->obvious = true;
 		update_smart_learn(context->mon, context->p, OF_HOLD_LIFE, 0, -1);
 		if (context->p->is_dead) return;
 	} else {
+		assert(context->t_mon);
+		display_blow_message_vs_monster(context->method,
+			context->m_name, context->t_mon->midx);
 		(void) mon_take_nonplayer_hit(context->damage, context->t_mon,
 									  MON_MSG_NONE, MON_MSG_DIE);
 		return;
@@ -546,6 +637,14 @@ static void melee_effect_experience(melee_effect_handler_context_t *context,
  */
 static void melee_effect_handler_NONE(melee_effect_handler_context_t *context)
 {
+	if (context->p) {
+		display_blow_message_vs_player(context->method,
+			context->m_name, context->p, 0);
+	} else {
+		assert(context->t_mon);
+		display_blow_message_vs_monster(context->method,
+			context->m_name, context->t_mon->midx);
+	}
 	context->obvious = true;
 	context->damage = 0;
 }

src/mon-blows.h
@@ -58,10 +58,13 @@ typedef struct melee_effect_handler_context_s {
 	const int rlev;
 	const struct blow_method *method;
 	const int ac;
-	const char *ddesc;
+	const char *ddesc;		/* short monster name for death
+						messages; unused if target is
+						not the player */
 	bool obvious;
 	bool blinked;
 	int damage;
+	const char *m_name;		/* monster name for messaging */
 } melee_effect_handler_context_t;
 
 /**
@@ -87,7 +90,7 @@ extern struct blow_effect *blow_effects;
 
 /* Functions */
 int blow_index(const char *name);
-char *monster_blow_method_action(struct blow_method *method, int midx);
+char *monster_blow_method_action(const struct blow_method *method, int midx);
 extern melee_effect_handler_f melee_handler_for_blow_effect(const char *name);
 
 #endif /* MON_BLOWS_H */

commit 8433b3df51755e9161ab160e0d97f94fe9e51750

Set player race for new monster message with damage
Corrects regression from febb4aed296bcef0bc374d604715ad2c19b8892f .  Resolves https://github.com/NickMcConnell/FAangband/issues/426 .

src/mon-msg.c
@@ -300,6 +300,7 @@ bool add_monster_message_show_damage(struct monster *mon, int msg_code,
 			!stack_message(mon, msg_code, flags, damage) &&
 			size_mon_msg < MAX_STORED_MON_MSG) {
 		mon_msg[size_mon_msg].race = mon->race;
+		mon_msg[size_mon_msg].p_race = mon->player_race;
 		mon_msg[size_mon_msg].flags = flags;
 		mon_msg[size_mon_msg].msg_code = msg_code;
 		mon_msg[size_mon_msg].count = 1;