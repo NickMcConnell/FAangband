NickMcConnell/FAangband

commit b71d5a96564f9cbe44ab42d7cd379ad4e9272537

Update copyright year in documentation footer to 2025 (#6267)

docs/conf.py
@@ -19,8 +19,9 @@ import os
 # -- Project information -----------------------------------------------------
 
 project = "FAangband"
-copyright = "2019, FAangband developers past and present"
-author = "FAangband developers past and present"
+copyright = "2025, Angband developers past and present"
+author = "Angband developers past and present"
+)
 
 # The full version, including alpha/beta/rc tags
 # There's extra clutter here (and for the HTML theme) to allow conf.py to

commit 75639bc3bbbd419736de8e7fede419a2be33eedc

docs - remove stray parenthesis from conf.py
That's a regression introduced by b71d5a96564f9cbe44ab42d7cd379ad4e9272537 .

docs/conf.py
@@ -21,7 +21,6 @@ import os
 project = "FAangband"
 copyright = "2025, Angband developers past and present"
 author = "Angband developers past and present"
-)
 
 # The full version, including alpha/beta/rc tags
 # There's extra clutter here (and for the HTML theme) to allow conf.py to